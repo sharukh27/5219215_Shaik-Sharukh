928205d7baddcf1aaf407dbad0dd1e3b67998ac8
shaik sharukh <sharukhsalvatore@gmail.com>
2025-07-27 22:40:14 +0530

    added python fies

---
+++ python1.py
@@ -0,0 +1,6 @@
+a=1
+b=2
+if a<b:
+    print("condition is correct")
+else:
+    print("condition is not correct")
---
+++ python2.py
@@ -0,0 +1,7 @@
+a=29
+b=9
+if a/b==3:
+    print("it is divisibe by a")
+else:
+    print("give another number for a")
+    
